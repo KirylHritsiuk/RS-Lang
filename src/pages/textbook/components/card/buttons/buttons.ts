57dc0456eef1f9949270cdd40203dfc19e14b1d1
KirylHritsiuk <pishime2017@gmail.com>
2022-09-07 19:01:47 +0300

    fix: response update word

--- src/pages/textbook/components/card/buttons/buttons.ts
+++ src/pages/textbook/components/card/buttons/buttons.ts
@@ -26,14 +26,23 @@ export class CardButtons extends Block {
     this.easy = new Button(data, CardButtons.textObject.difficultEasy).render();
     this.container.append(this.difficult, this.easy);
     [this.difficult, this.easy].forEach((btn) => {
-      btn.addEventListener('click', () => {
+      btn.addEventListener('click', async () => {
         this.addedCard();
-        api.createUserWord(
-          this.user,
-          this.token,
-          data._id,
-          { difficulty: (btn.textContent as string) },
-        );
+        if (data.userWord === undefined) {
+          await api.createUserWord(
+            this.user,
+            this.token,
+            data._id,
+            { difficulty: (btn.textContent as string) },
+          );
+        } else {
+          await api.updateUserWord(
+            this.user,
+            data._id,
+            this.token,
+            { difficulty: (btn.textContent as string) },
+          );
+        }
       });
     });
   }
