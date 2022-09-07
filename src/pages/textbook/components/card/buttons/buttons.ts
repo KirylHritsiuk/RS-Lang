e6a0d5814ff33061a6c68e4597bc3ef2205d6350
KirylHritsiuk <pishime2017@gmail.com>
2022-09-07 18:28:24 +0300

    fix: buttons hidden

--- src/pages/textbook/components/card/buttons/buttons.ts
+++ src/pages/textbook/components/card/buttons/buttons.ts
@@ -8,6 +8,7 @@ export class CardButtons extends Block {
     containerClass: 'card-buttons',
     difficultHard: 'hard',
     difficultEasy: 'easy',
+    difficultNormal: 'normal',
   };
 
   protected difficult: HTMLElement;
@@ -17,7 +18,8 @@ export class CardButtons extends Block {
   constructor(protected data: IWord) {
     super();
     this.container.className = CardButtons.textObject.containerClass;
-    if (this.user === '' || data.userWord !== undefined) {
+    if (this.user === ''
+    || data.userWord?.difficulty !== CardButtons.textObject.difficultNormal) {
       this.container.classList.add(Block.modificationClass.displayNone);
     }
     this.difficult = new Button(data, CardButtons.textObject.difficultHard).render();
